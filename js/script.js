3c232334671dde4d4053914c149f57b53a5661e8
Nicolae Pop <nicupop729@gmail.com>
2021-10-18 18:37:58 -0300

    implement the functionality to remove book

--- js/script.js
+++ js/script.js
@@ -1,24 +1,21 @@
-// STEP 1: create the html skeleton of the website - completed
-
-// STEP 2: Create an array of objects containing the collection of books
-let books = [];
-
 const booksElement = document.querySelector(".books");
 const titleInput = document.querySelector(".title-input");
 const authorInput = document.querySelector(".author-input");
 const addBookBtn = document.querySelector("#addBtn");
 
-// STEP 3: Create a function that adds new books to the array in step 2
+let books = [];
 
-
-addBookBtn.addEventListener("click", (e) => {
-  // e.preventDefault();
+addBookBtn.addEventListener("click", () => {
   addBooks();
 });
 
+const saveToLocalStorage = (books) => {
+  localStorage.setItem("books", JSON.stringify(books));
+};
+
 const getExistingBooks = () => {
   return JSON.parse(localStorage.getItem("books"));
-}
+};
 
 const addBooks = () => {
   let title = titleInput.value;
@@ -34,34 +31,34 @@ const addBooks = () => {
 
   books.push(newBook);
 
-  localStorage.setItem("books", JSON.stringify(books));
+  saveToLocalStorage(books);
   books = [];
 
   titleInput.value = authorInput.value = "";
 };
 
 const displayBooks = () => {
-
   if (getExistingBooks()) {
-    getExistingBooks().forEach((book) => {
-      booksElement.innerHTML += `
+    getExistingBooks().forEach((book, index) => {
+      const textHtml = `
       <div class="book">
       <p class="title">${book.title}</p>
       <p class="author">${book.author}</p>
-      <button id="remove-btn">Remove</button>
+      <button id="remove-btn" onclick="removeBook(${index})">Remove</button>
       <hr class="bottom-border" />
       </div>`;
+
+      booksElement.insertAdjacentHTML("afterbegin", textHtml);
     });
   }
 };
 
 displayBooks();
 
-// 3.1: Store individual books in localstorage
-// 3.2: Add functionality to the add button to add new books to the array and display it
-
-// STEP 4: Create a function to remove books from the array( book collection)
-// 4.1 : When the remove button is clicked, remove the books from the array and stop displaying it
-// 4.2 : Remove the book item from localStorage
-
-// STEP 5: Display the books in the top part of the page
+const removeBook = (index) => {
+  const filterBooks = getExistingBooks().filter(
+    (existingBook, i) => index !== i
+  );
+  saveToLocalStorage(filterBooks);
+  location.reload();
+};
