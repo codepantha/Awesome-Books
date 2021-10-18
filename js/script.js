f722bba4a3b4587bde218882ceffdfae2583bb4b
codepanther <pezechigozirim@gmail.com>
2021-10-18 18:15:45 +0100

    Outline steps

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// STEP 1: create the html skeleton of the website
+
+// STEP 2: Create an array of objects containing the collection of books
+
+// STEP 3: Create a function that adds new books to the array in step 2
+    // 3.1: Store individual books in localstorage
+    // 3.2: Add functionality to the add button to add new books to the array and display it
+
+
+// STEP 4: Create a function to remove books from the array( book collection)
+    // 4.1 : When the remove button is clicked, remove the books from the array and stop displaying it
+    // 4.2 : Remove the book item from localStorage
+
+// STEP 5: Display the books in the top part of the page
+
