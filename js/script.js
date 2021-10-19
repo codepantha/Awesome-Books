fdea3dbb9bb87af2e4e9920e6079830d08741c99
Nicolae Pop <nicupop729@gmail.com>
2021-10-19 12:27:12 -0300

    start refactoring code to use classes

--- js/script.js
+++ js/script.js
@@ -5,70 +5,119 @@ const titleInput = document.querySelector('.title-input');
 const authorInput = document.querySelector('.author-input');
 const addBookBtn = document.querySelector('#addBtn');
 
-let books = [];
-
-const saveToLocalStorage = (books) => {
-  localStorage.setItem('books', JSON.stringify(books));
-};
-
-const getExistingBooks = () => JSON.parse(localStorage.getItem('books'));
-
-function generateRandomId() {
-  return Math.random().toString(20).substr(2, 20);
-}
-
-const addBooks = () => {
-  const title = titleInput.value;
-  const author = authorInput.value;
-  const id = generateRandomId();
-
-  const newBook = { title, author, id };
-
-  // get existing books from localStorage
-  if (getExistingBooks()) {
-    getExistingBooks().forEach((existingBook) => {
-      books.push(existingBook);
-    });
+class Book {
+  contructor(title = null, author = null) {
+    this.title = title;
+    this.author = author;
+    this.books = [];
   }
 
-  books.push(newBook);
+  // eslint-disable-next-line class-methods-use-this
+  saveToLocalStorage(books) {
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 
-  saveToLocalStorage(books);
-  books = [];
+  // eslint-disable-next-line class-methods-use-this
 
-  titleInput.value = '';
-  authorInput.value = '';
-};
+  generateRandomId() {
+    return Math.random().toString(20).substr(2, 20);
+  }
 
-addBookBtn.addEventListener('click', () => {
-  addBooks();
+  getExistingBooks() {
+    JSON.parse(localStorage.getItem('books'));
+  }
+
+  addBooks() {
+    const newBook = {
+      title: this.title,
+      author: this.author,
+      id: this.generateRandomId(),
+    };
+
+    // get existing books from localStorage
+    if (this.getExistingBooks()) {
+      this.getExistingBooks().forEach((existingBook) => {
+        this.books.push(existingBook);
+      });
+    }
+
+    this.books.push(newBook);
+
+    this.saveToLocalStorage(this.books);
+    this.books = [];
+  }
+}
+
+// let books = [];
+
+// const saveToLocalStorage = (books) => {
+//   localStorage.setItem('books', JSON.stringify(books));
+// };
+
+// const getExistingBooks = () => JSON.parse(localStorage.getItem('books'));
+
+// function generateRandomId() {
+//   return Math.random().toString(20).substr(2, 20);
+// }
+
+// const addBooks = () => {
+//   const title = titleInput.value;
+//   const author = authorInput.value;
+//   const id = generateRandomId();
+
+//   const newBook = { title, author, id };
+
+//   // get existing books from localStorage
+//   if (getExistingBooks()) {
+//     getExistingBooks().forEach((existingBook) => {
+//       books.push(existingBook);
+//     });
+//   }
+
+//   books.push(newBook);
+
+//   saveToLocalStorage(books);
+//   books = [];
+
+//   titleInput.value = '';
+//   authorInput.value = '';
+// };
+
+addBookBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const book = new Book(titleInput.value, authorInput.value);
+  book.addBooks();
 });
 
-const displayBooks = () => {
-  if (getExistingBooks()) {
-    getExistingBooks().forEach((book) => {
-      const textHtml = `
-      <div class="book">
-      <p class="title">${book.title}</p>
-      <p class="author">${book.author}</p>
-      <button class="remove-btn" data-id=${book.id}>Remove</button>
-      <hr class="bottom-border" />
-      </div>`;
+// const displayBooks = () => {
+//   if (getExistingBooks()) {
+//     getExistingBooks().forEach((book) => {
+//       const textHtml = `
+//       <div class="book">
+//       <p class="title">${book.title}</p>
+//       <p class="author">${book.author}</p>
+//       <button class="remove-btn" data-id=${book.id}>Remove</button>
+//       <hr class="bottom-border" />
+//       </div>`;
 
-      booksElement.insertAdjacentHTML('afterbegin', textHtml);
-    });
-  }
-};
+//       booksElement.insertAdjacentHTML('afterbegin', textHtml);
+//     });
+//   }
+// };
 
-displayBooks();
+// displayBooks();
 
-function removeBook(bookId) {
-  const filterBooks = getExistingBooks().filter((existingBook) => existingBook.id !== bookId);
-  saveToLocalStorage(filterBooks);
-  window.location.reload();
-}
+// function removeBook(bookId) {
+//   const filterBooks = getExistingBooks().filter(
+//     (existingBook) => existingBook.id !== bookId
+//   );
+//   saveToLocalStorage(filterBooks);
+//   window.location.reload();
+// }
 
-// traverse through the remove buttons and add onclick event listeners
-Array.from(document.querySelectorAll('.remove-btn')).forEach((btn) => btn.addEventListener('click', () => {
-  removeBook(btn.dataset.id);
-}));
+// // traverse through the remove buttons and add onclick event listeners
+// Array.from(document.querySelectorAll('.remove-btn')).forEach((btn) =>
+//   btn.addEventListener('click', () => {
+//     removeBook(btn.dataset.id);
+//   })
+// );
